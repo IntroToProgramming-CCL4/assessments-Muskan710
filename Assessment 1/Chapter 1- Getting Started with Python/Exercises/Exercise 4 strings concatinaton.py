e7c76adff3e30fa2be2dd92d9a87b775fad2374b
Muskan710 <144933944+Muskan710@users.noreply.github.com>
2023-10-08 17:58:01 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise 4 strings concatinaton.py
@@ -0,0 +1,9 @@
+# print 3 different string variables 
+string1="Red"
+string2="Green"
+string3="pink"
+#Now print output as a one string 
+output_string=string1+ string2+ string3
+#the concatenation is the process of appending one string to the end of another string.
+#we concatenate strings by using + operator.
+print(output_string)
