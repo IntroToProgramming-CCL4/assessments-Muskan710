59267498397054935e433bba2c3f43ee9e7d3123
Muskan710 <144933944+Muskan710@users.noreply.github.com>
2023-10-08 17:54:57 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise 1 print specific format.py
@@ -0,0 +1,10 @@
+#print the string in a specific format.
+print("Twinkel,twinkel,little star,")
+#To print a string we must use parentheses and quotation mark.
+print("       How I wonder what you are!")
+print  ("             Up above the world so high,")
+print  ("             Like a diamond in the sky")
+# The above line will be printed as it is beacuse we give spaces to print the format.
+print("Twinkle,twinkle,little star,")
+print("       How I wounder what you are ")
+# Output will be printed as above where spaces are given those lines will be printed as.
