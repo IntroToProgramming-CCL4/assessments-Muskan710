06cc0b021bca20f6c60d7e79942c2ddce5d61e37
Muskan710 <144933944+Muskan710@users.noreply.github.com>
2023-10-27 10:12:41 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/Exercise 3 stripping names.py
@@ -0,0 +1,18 @@
+#"\t" funtion is used to represent some whitespaces, "\n" funtion is use to print newline.
+name="\t""palwasha is very smart\n"
+print(name)
+
+#lstrip function removes any leading whitespaceson from left in the string.
+name="   palwasha "
+name.lstrip()
+print(name)
+
+#rstrip function removes any leading trailing whitespaces from the right in string.
+name="palwasha     "
+name.rstrip()
+print(name)
+
+#strip function is used to  removes any leading or trailing whitespaces from both left and right in string.
+name="  Palwasha   "
+name.strip()
+print(name)
