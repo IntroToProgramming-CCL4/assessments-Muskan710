f1822f92f5251a7459ae2fe0cb9e6dfe96fc054e
Muskan710 <144933944+Muskan710@users.noreply.github.com>
2023-10-27 12:54:41 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/Exercise 4 favorite number.py
@@ -0,0 +1,8 @@
+# Assign  your favorite number to a variable favorite_number
+Favorite_number = 6
+#Now create a meassage that reveals your favorite number 
+#
+message =" My favorite number is " + str(Favorite_number) 
+#printing the message
+print(message)
+
