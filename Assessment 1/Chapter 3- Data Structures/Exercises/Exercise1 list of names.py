920c45b91c14669b3762b1796be30133daf0b659
Muskan710 <144933944+Muskan710@users.noreply.github.com>
2023-10-27 19:26:15 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Exercise1 list of names.py
@@ -0,0 +1,7 @@
+#create a list of your friends names.
+names =["Mariyam","Andy","Hina","Anaya","Ana"]
+# Now we use a for loop to access and print each name at a time. 
+for name in names:
+    print(name)
+#we use print statement to print each friends name.
+# the loop continues untill it has gone through all the names in the list.
