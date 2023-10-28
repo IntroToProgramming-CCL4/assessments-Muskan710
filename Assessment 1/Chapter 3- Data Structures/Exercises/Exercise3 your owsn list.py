b25816e187693c1f0d1c7695422ccb2cb1c2acad
Muskan710 <144933944+Muskan710@users.noreply.github.com>
2023-10-28 13:03:44 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Exercise3 your owsn list.py
@@ -0,0 +1,8 @@
+#create a list of your favorite mood of transportation.
+transportation_mood = ["Tesla car", "Toyota car", "Mercedes Benz", "Bugatti", "Ferrari car"]
+#We use a for loop to iterste through each items in the transportation_mood list.
+for item in transportation_mood: 
+#creating a message using formatted string (f_string)
+    statement = f"I would like to own a {item}."
+#finially printing statement of each item.  
+    print(statement)
