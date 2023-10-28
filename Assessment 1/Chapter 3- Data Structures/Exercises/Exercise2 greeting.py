5a10be870887b93ecaede14fe1cd6609a6f41575
Muskan710 <144933944+Muskan710@users.noreply.github.com>
2023-10-28 13:03:16 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Exercise2 greeting.py
@@ -0,0 +1,11 @@
+#create a list and assign a personalized message.
+names = ["Mariyam", "Andy", "Hina", "Anaya", "Ana"]
+#use a for loop to access and print a personalized message to each friend.
+# we create a personalized message for each friend by using formatted string (f-string.)
+for name in names:
+    message = f"Hi, {name}! Have a wonderful day a head."
+#printing personalized message to your friends.
+    print(message)
+
+
+
