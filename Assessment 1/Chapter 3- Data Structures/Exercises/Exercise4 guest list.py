6f4ef0432c4217df41e6adb8e11398a5ed123a74
Muskan710 <144933944+Muskan710@users.noreply.github.com>
2023-10-28 13:08:37 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Exercise4 guest list.py
@@ -0,0 +1,9 @@
+#create a list to invite friends for dinner.
+invitees = ["Hina","Mariyam","Ifrah"]
+# using for loop to iterate through each (person) in the invitees list.
+#creating a message using formatted string(f_string).
+for person in invitees:
+    invitation = f"Dear {person},you are invited for dinner at my place. Please join us for a wonderful evening. "
+#Now printing invitation for each person. 
+    print(invitation)
+
