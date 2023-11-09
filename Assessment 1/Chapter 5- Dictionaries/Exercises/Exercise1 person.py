88d77cceed2259b81598e6ef760bf6e9681ffa1c
Muskan710 <144933944+Muskan710@users.noreply.github.com>
2023-11-09 17:22:17 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 5- Dictionaries/Exercises/Exercise1 person.py
@@ -0,0 +1,10 @@
+# create a dictionary to store information about a person.
+person={'first_name':'Anaya','last_name':'khan','age':15, 'city':'Dubai'}
+
+# we will print each piece of information stored in the dictionary using the keys.
+# The person['first_name] which will accesses the value associated with the ('first_name') key in the dictionary.
+print("First Name:",person['first_name'])
+# and so we will use for other keys as following. this will retrieve and display each piecenof information about the person.
+print("Last Name:",person['last_name'])
+print("Age:",person['age'])
+print("City:",person['city'])
