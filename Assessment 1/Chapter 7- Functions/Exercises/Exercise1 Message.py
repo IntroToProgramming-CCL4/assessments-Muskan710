a7ba842f5c2d35719c5c5cd10e41eb426a4b954b
Muskan710 <144933944+Muskan710@users.noreply.github.com>
2023-11-09 17:01:12 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 7- Functions/Exercises/Exercise1 Message.py
@@ -0,0 +1,7 @@
+# Write function to display a message.
+# we use def statement which creates a function object and assigns it to a name.
+def display_message():
+#printing the message.
+    print("In this chapter, I am learing about function ,def and about parameters.")
+#call the function to display the  message.
+display_message()    
