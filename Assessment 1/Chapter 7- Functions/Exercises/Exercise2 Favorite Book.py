3ec58e5c174da62f3b7d2364bf3e9bd4bb7ae7c6
Muskan710 <144933944+Muskan710@users.noreply.github.com>
2023-11-09 17:01:40 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 7- Functions/Exercises/Exercise2 Favorite Book.py
@@ -0,0 +1,5 @@
+# for this program will use def statement
+def favorite_book(title):
+    print("One of my favorite book is",title)
+#calling the function with a book title
+favorite_book("DIARY of a Wimpy Kid.")
