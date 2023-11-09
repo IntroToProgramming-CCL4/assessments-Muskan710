c11b02f62f40eecaebdae7b2dc1fabdaaea61ed5
Muskan710 <144933944+Muskan710@users.noreply.github.com>
2023-11-09 17:42:37 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 4- Control Flow/Exercises/Exercise5 favoite fruit.py
@@ -0,0 +1,28 @@
+# Make a list of your 3 favorite fruits.
+favorite_fruits= ['graps','orange','mango']
+
+# write 5 if statements to check for specific fruits
+#we will use if statements to check if 'graps' is in favorite_fruits list.
+if 'graps' in favorite_fruits:
+#if 'graps' is found in favorite_fruits list it should print the following statement.
+    print("You really like graps!")
+
+# Same if statement we will use to check for 'apple' in list (favorite_fruits).
+if 'apple' in favorite_fruits:
+# if apple is found in the list favorite_fruits it should print the following statement.however apple is not in list so 'if' block will not be executed.
+    print("You really like apples!")
+
+# we will use if statements  again now to check if 'orange' is in favorite_fruits list.
+if 'orange' in favorite_fruits:
+#if 'orange' is found in favorite_fruits list it should print the following statement.
+    print("You really like orange!")
+
+# we will use if statements now to check if 'kiwi' is in favorite_fruits list.
+if 'kiwi' in favorite_fruits:
+# if kiwi is found in the list favorite_fruits it should print the following statement.however kiwi is not in list so 'if' block will not be executed.
+    print("You really like kiwi!")
+
+# we will use the if statements now to check if 'mango' is in favorite_fruits list.
+if 'mango' in favorite_fruits:
+#if 'mango' is found in favorite_fruits list it should print the following statement.
+    print("You really like mango!")
