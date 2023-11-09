c43768b206821b2c5c42fee92badff111a0b46f7
Muskan710 <144933944+Muskan710@users.noreply.github.com>
2023-11-09 17:40:30 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 4- Control Flow/Exercises/Exercise1 Alien color.py
@@ -0,0 +1,20 @@
+#write an if statement to test wheather the aliens color is green.
+#create a variable called as alien_color.
+
+alien_color = "green"
+#the if statement will now check if the alien_color is equal to green .
+if alien_color == "green":
+#if its true than print  the following message.
+    print("Player just earned 5 points.")
+
+
+#version2: write a code that fails the if test (alien_color is not green).
+alien_color ="Red"
+#the if statement will check if the color is equal to green.
+if alien_color =="green":
+#if is not equal so the code inside the if block will not be executed leading to no output.
+    print("Player just earned 10 points.")
+
+
+#version 1 is true as the color is green.
+#version 2 is false as the color is red.
