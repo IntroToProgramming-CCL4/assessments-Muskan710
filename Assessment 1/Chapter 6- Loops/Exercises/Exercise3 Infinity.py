5a3e34f034316bde273a7bddbeb6e702c4fbebf2
Muskan710 <144933944+Muskan710@users.noreply.github.com>
2023-11-09 17:54:35 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 6- Loops/Exercises/Exercise3 Infinity.py
@@ -0,0 +1,7 @@
+#we will use while loop for this programme.
+# A while loop is  that runs indefinitely and we will print a message thats never ends.
+while True:
+    print("Like programming ,its fun to use codes.")
+    
+#so here we did not wrote any code to stop this programme so it will continue to print untill we 
+# press Ctrl-C or  to close the window diplaying the output.
